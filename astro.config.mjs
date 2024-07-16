d3ac56d7330e7ade6997dfa9f82a5b83f3f2c8e3
olinfernandes <olin.f@live.com>
2024-07-17 02:39:28 +0530

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap(), compress(), icon()],
+  integrations: [tailwind(), react(), sitemap(), compress(), icon({iconDir: 'src/content/icons'})],
   output: "server",
   image: {
     service: passthroughImageService()
