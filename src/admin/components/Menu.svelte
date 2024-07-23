0a5533248f6c70f2bfb0637e0fcbdedde82d9ee5
olinfernandes <olin.f@live.com>
2024-07-23 22:41:59 +0530

    up

--- src/admin/components/Menu.svelte
+++ src/admin/components/Menu.svelte
@@ -1,3 +1,9 @@
-<ul class='w-full flex flex-col items-center'>
-  <li class='w-full grid place-self-center place-items-center'>Menu Item</li>
+<script lang="ts">
+  const items = ["item 1", "item 2", "item 3"];
+</script>
+
+<ul class="w-full flex flex-col items-center">
+  {#each items as item}
+    <li class="w-full grid place-self-center place-items-center">{item}</li>
+  {/each}
 </ul>
