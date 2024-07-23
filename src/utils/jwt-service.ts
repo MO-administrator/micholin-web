e7fdc217b3cce3a0166201d7fc2f9ce73a08c803
olinfernandes <olin.f@live.com>
2024-07-23 12:44:18 +0530

    up

--- src/utils/jwt-service.ts
+++ src/utils/jwt-service.ts
@@ -6,26 +6,37 @@ import JWT from 'jsonwebtoken';
  */
 export default class JwtService {
   private readonly jwt = JWT;
-  private readonly secret = import.meta.env.TOKEN_SECRET || 'randomsecret';
+  private readonly secret = import.meta.env.TOKEN_SECRET || "randomsecret";
   /**
    * Takes a string and returns an encoded jwt
-   * @param userId token source
+   * @param {string|Buffer|object} userId token source
    * @returns {string}
    */
-  public generateToken = (userId: string): string => {
-    const secret = this.secret;
+  public generateToken = (
+    userId: string | Buffer | object,
+    secretOrPrivateKey?: JWT.Secret,
+    options?: JWT.SignOptions
+  ): string => {
+    const secret = secretOrPrivateKey || this.secret;
     return this.jwt.sign({ data: { userId } }, secret, {
       expiresIn: "16h",
+      ...options,
     });
   };
   /**
    * Decodes a token and returns the json
    * @param token source token
-   * @returns {JWT.Jwt}
+   * @returns {JWT.Jwt|JWT.JwtPayload|string}
    */
-  public decodeToken = (token: string): JWT.Jwt => {
-    return this.jwt.verify(token, this.secret, {
+  public decodeToken = (
+    token: string,
+    secretOrPublicKey?: JWT.Secret,
+    options?: JWT.DecodeOptions
+  ): JWT.Jwt | JWT.JwtPayload | string => {
+    const secret = secretOrPublicKey || this.secret;
+    return this.jwt.verify(token, secret, {
       complete: true,
+      ...options,
     });
   };
 }
