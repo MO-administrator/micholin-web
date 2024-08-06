c4d0feaabc3a36d1f0b597c6467c6e6ee0ec4659
olinfernandes <olin.f@live.com>
2024-08-07 00:06:46 +0530

    up

--- src/test/GetRandomItem.test.ts
+++ src/test/GetRandomItem.test.ts
@@ -1,5 +1,5 @@
 import { getRandomItem } from '@/utils/get-random-item';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 describe('getRandomItem()', () => {
   let mockArray = new Array();
